0af5b2d56b5c43fa9667c123f6ba864a0b959c16
Mizoleni <Mizoleni@Gmail.com>
2016-11-25 09:51:13 +0000

    Add files via upload

---
+++ celsius_to_farenheit.py
@@ -0,0 +1,14 @@
+#forumla's
+Fahrenheit = int(raw_input("Enter a temperature in Fahrenheit: "))
+
+
+Celsius = (Fahrenheit - 32) * 5.0/9.0
+
+print "Temperature:", Fahrenheit, "Fahrenheit =", Celsius, "C"
+
+Celsius = int(raw_input("Enter a temperature in Celsius: "))
+
+Fahrenheit = (Celsius * 9.0/5.0) + 32
+
+print "Temperature:", Celsius, "Celsius =", Fahrenheit, "F"
+
